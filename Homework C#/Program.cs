e192f00f9d2fb4e0c81194e1bd1722bb948d3920
Anton Hoviadovskyi <anton.hoviadovskyi@hisaus.org>
2025-10-23 09:14:51 +0300

    Add project files.

---
+++ Homework C#/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number(number should be less than 1 000 000 000): ");
+            int num = int.Parse(Console.ReadLine()!);
+
+            Console.WriteLine($"Sum of the digits of a number: {SumOfDigits(num)}");
+
+            Console.WriteLine($"Number of units in binary notation: {CountOnesInBinary(num)}");
+
+            Console.WriteLine($"The result of digitizing the number N!: {Digitization(num)}");
+        }
+
+
+        static int SumOfDigits( int num) 
+        {
+            int sum = 0;
+            while (num > 0) 
+            {
+                sum += num % 10;
+                num /= 10;
+            }
+            return sum;
+        }
+
+        static int CountOnesInBinary(int num) 
+        {
+            int count = 0;
+
+            while (num > 0) 
+            {
+                if((num & 1) == 1) 
+                {
+                    count++;
+                }
+                num >>=1;
+            }
+            return count;
+        }
+
+        static int Digitization(int num)
+        {
+            if (num == 1 || num == 0) return 1;
+            if (num == 2) return 2;
+            if (num == 3 || num == 4) return 6;
+            if (num == 5) return 3;
+            return 9;
+        }
+    }
+}
